12428a3955625018c6d576e9a18244c60456dd26
William Kolodziejski <wkolodziejski@inf.ufpel.edu.br>
2021-09-17 17:32:04 -0300

    chat

--- Assets/Scripts/Chat/ChatManager.cs
+++ Assets/Scripts/Chat/ChatManager.cs
@@ -1,4 +1,8 @@
+using System;
+using Network;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Chat
 {
@@ -7,12 +11,37 @@ namespace Chat
         
         [SerializeField] public GameObject container;
         [SerializeField] public GameObject prefab;
+        [SerializeField] public TMP_InputField msg;
+
+        private Message lastMessage;
 
         public void ShowMessage(bool isMine, string sender, string text)
         {
             Message message = Instantiate(prefab, container.transform).GetComponent<Message>();
             message.Setup(isMine, sender, text);
+
+            lastMessage = message;
         }
-        
+
+        public void SendMessage()
+        {
+            string message = msg.text.Trim();
+
+            if (message.Length <= 0) return;
+            
+            if (!message.EndsWith("?"))
+                message += "?";
+
+            msg.Select();
+            msg.text = "";
+            ShowMessage(true, "Me", message);
+            Client.Instance.Send(SenderParser.ParseQuestion("William", message));
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+                SendMessage();
+        }
     }
 }
